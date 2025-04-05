e0c48d6dec005d5e186be572d61ea076f4603b1d
perch2 <perch12342@gmail.com>
2025-04-06 05:19:15 +0700

    game

--- game.py
+++ game.py
@@ -6,17 +6,14 @@ class SecretWord:
     def __init__(self):
         self.word = random.choice(wordlist)
         self.attempts = 30
-
     def userInputted(self,message): #call this function when user inputs something
         attempts -= 1
         if attempts < 0:
             return "out_of_attempts_message"
         output = get_user_input(message)
+        if self.word in output.split():
+            return output + "\nyou made it etc."
         return output
-    def userWordGuess(self,word): #call this function when user guesses a word
-        if self.word == word:
-            return f"Congratulations! The word was {self.word}, you guessed it with {self.attempts} remaining"
-        return f"incorrect"
     def button(self,id):
         match id:
             case "RESET":
